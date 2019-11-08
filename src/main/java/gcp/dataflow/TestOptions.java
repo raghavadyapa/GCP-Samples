24bf362da9d48d3522d0d3964d0d49bd0755f8e2
raghavadyapa <raghava.dyapa@techolution.com>
2019-11-08 16:19:56 +0530

    commented this interface

--- src/main/java/gcp/dataflow/TestOptions.java
+++ src/main/java/gcp/dataflow/TestOptions.java
@@ -1,20 +1,20 @@
-package gcp.dataflow;
-
-
-import org.apache.beam.sdk.options.PipelineOptions;
-import org.apache.beam.sdk.options.StreamingOptions;
-import org.apache.beam.sdk.options.ValueProvider;
-
-public interface TestOptions extends PipelineOptions, StreamingOptions {
-
-  ValueProvider<String> getInputTopic();
-  void setInputTopic(ValueProvider<String> value);
-
-
-  Integer getWindowSize();
-  void setWindowSize(Integer value);
-
-  ValueProvider<String> getOutputLocation();
-  void setOutputLocation(ValueProvider<String> values);
-
-}
+//package gcp.dataflow;
+//
+//
+//import org.apache.beam.sdk.options.PipelineOptions;
+//import org.apache.beam.sdk.options.StreamingOptions;
+//import org.apache.beam.sdk.options.ValueProvider;
+//
+//public interface TestOptions extends PipelineOptions, StreamingOptions {
+//
+//  ValueProvider<String> getInputTopic();
+//  void setInputTopic(ValueProvider<String> value);
+//
+//
+//  Integer getWindowSize();
+//  void setWindowSize(Integer value);
+//
+//  ValueProvider<String> getOutputLocation();
+//  void setOutputLocation(ValueProvider<String> values);
+//
+//}
