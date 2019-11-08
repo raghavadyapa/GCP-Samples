3f089964b66b62ce08350b30a8b53746a95eb924
raghavadyapa <raghava.dyapa@techolution.com>
2019-11-08 14:14:09 +0530

    testing options

---
+++ src/main/java/gcp/dataflow/TestOptions.java
@@ -0,0 +1,20 @@
+package gcp.dataflow;
+
+
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.StreamingOptions;
+import org.apache.beam.sdk.options.ValueProvider;
+
+public interface TestOptions extends PipelineOptions, StreamingOptions {
+
+  ValueProvider<String> getInputTopic();
+  void setInputTopic(ValueProvider<String> value);
+
+
+  Integer getWindowSize();
+  void setWindowSize(Integer value);
+
+  ValueProvider<String> getOutputLocation();
+  void setOutputLocation(ValueProvider<String> value);
+
+}
