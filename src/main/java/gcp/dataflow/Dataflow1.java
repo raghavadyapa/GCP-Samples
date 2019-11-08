369a21ff55807002fc01d99e3f965a169ef373ba
raghavadyapa <raghava.dyapa@techolution.com>
2019-11-08 16:18:40 +0530

    testing option

--- src/main/java/gcp/dataflow/Dataflow1.java
+++ src/main/java/gcp/dataflow/Dataflow1.java
@@ -3,12 +3,31 @@ package gcp.dataflow;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
-import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.*;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.joda.time.Duration;
 
 public class Dataflow1 {
+
+  interface TestOptions extends PipelineOptions{
+
+    @Description("The Cloud Pub/Sub topic to read from.")
+    @Validation.Required
+    ValueProvider<String> getInputTopic();
+    void setInputTopic(ValueProvider<String> value);
+
+    @Description("Output file's window size in number of minutes.")
+    @Default.Integer(1)
+    Integer getWindowSize();
+    void setWindowSize(Integer value);
+
+    @Description("Path of the output file including its filename prefix.")
+    @Validation.Required
+    ValueProvider<String> getOutputLocation();
+    void setOutput(ValueProvider<String> value);
+
+  }
 
   public static void main(String[] args) {
 
